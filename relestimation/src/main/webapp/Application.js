e55bb79be4b826daf24b60e0c68db22a50a9def4
Jaishankararam Venkatesan <jaiishankar@gmail.com>
2014-08-02 15:57:14 -0700

    Modify the code to check if the current user is still available and allowed to auto log on

--- relestimation/src/main/webapp/Application.js
+++ relestimation/src/main/webapp/Application.js
@@ -44,9 +44,11 @@ Ext.application({
     launch: function() {
         estools.utils.reloadFromCacheIfExists();
         if (globalvar.currentUserId && globalvar.currentUserId > 0) {
-            Ext.create('estools.view.Viewport');
+            //we need to see if still that user is present in the DB and if not
+            // clear the local cache and re-route to login page.
+            this.validateAndShowApplicationPage(globalvar.currentUserId);
         } else {
-            Ext.create('estools.view.logon.LogonWindow').show();
+            this.showLogonWindow();
         }
     },
     // Application initialization stuff
@@ -54,5 +56,28 @@ Ext.application({
         // Attach listener to application wide events
         this.control({
         });
+    },
+    validateAndShowApplicationPage: function(userId) {
+        Ext.Ajax.request({
+            method: 'GET',
+            url: './v1/users/' + userId,
+            headers: {
+                'Accept': 'application/json'
+            },
+            scope: this,
+            success: function(response, options) {
+                var responseData = Ext.decode(response.responseText);
+                if (responseData.success) {
+                    Ext.create('estools.view.Viewport');
+                }
+                else {
+                    this.showLogonWindow();
+                }
+            }
+        });
+    },
+    showLogonWindow: function() {
+        estools.utils.clearCache();
+        Ext.create('estools.view.logon.LogonWindow').show();
     }
 });
