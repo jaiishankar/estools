42f567aee65f25550733d199ec43612d52ea5caa
Jaishankararam Venkatesan <jaiishankar@gmail.com>
2014-08-17 11:05:46 -0700

    Feature DB actions added

--- relestimation/src/main/java/edu/aspen/capstone/estimation/relative/dao/FeatureDAO.java
+++ relestimation/src/main/java/edu/aspen/capstone/estimation/relative/dao/FeatureDAO.java
@@ -1,15 +1,19 @@
-/*
- *  
- *  
- * 
- */
-
 package edu.aspen.capstone.estimation.relative.dao;
+
+import edu.aspen.capstone.estimation.relative.entity.Feature;
 
 /**
  *
  * @author jaiishankar
  */
 public interface FeatureDAO {
+
+    Feature add(Feature fea);
+
+    Feature update(Feature fea);
+
+    Feature get(Integer fId);
     
+    Boolean delete(Integer fId);
+
 }
--- relestimation/src/main/java/edu/aspen/capstone/estimation/relative/dao/FeatureDAOImpl.java
+++ relestimation/src/main/java/edu/aspen/capstone/estimation/relative/dao/FeatureDAOImpl.java
@@ -1,5 +1,9 @@
 package edu.aspen.capstone.estimation.relative.dao;
 
+import edu.aspen.capstone.estimation.relative.entity.Feature;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,5 +14,52 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository("FeatureDAO")
 @Transactional
 public class FeatureDAOImpl implements FeatureDAO {
-    
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Feature add(Feature fea) {
+        try {
+            sessionFactory.getCurrentSession().saveOrUpdate(fea);
+            sessionFactory.getCurrentSession().flush();
+            sessionFactory.getCurrentSession().refresh(fea);
+            return fea;
+        } catch (HibernateException hbe) {
+            hbe.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public Feature update(Feature fea) {
+        try {
+            return this.add(fea);
+        } catch (HibernateException hbe) {
+            hbe.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public Feature get(Integer fId) {
+        try {
+            return null;
+        } catch (HibernateException hbe) {
+            hbe.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public Boolean delete(Integer fId) {
+        try {
+            sessionFactory.getCurrentSession().delete(this.get(fId));
+            return true;
+        } catch (HibernateException hbe) {
+            hbe.printStackTrace();
+            return null;
+        }
+    }
+
 }
